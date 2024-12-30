c5e852cd9e3a04025d22a8fb05e794a7c32a229d
Arun Prajapati <arunkp1122@gmail.com>
2024-12-30 16:16:48 +0545

    Update config for production

--- coseli/medusa-config.ts
+++ coseli/medusa-config.ts
@@ -3,7 +3,15 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
+	admin: {
+		backendUrl: process.env.MEDUSA_BACKEND_URL,
+	},
 	projectConfig: {
+		workerMode: process.env.MEDUSA_WORKER_MODE as
+			| "shared"
+			| "worker"
+			| "server",
+
 		databaseUrl: process.env.DATABASE_URL,
 		http: {
 			storeCors: process.env.STORE_CORS!,
