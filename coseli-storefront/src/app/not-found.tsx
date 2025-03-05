3bcff1b684bb4baac8e6b46ba68734cc68891924
Arun Prajapati <arunkp1122@gmail.com>
2025-03-05 22:53:51 +0545

    Test runner

--- coseli-storefront/src/app/not-found.tsx
+++ coseli-storefront/src/app/not-found.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "404",
   description: "Something went wrong",
 }
-
+//test
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)] px-8">
